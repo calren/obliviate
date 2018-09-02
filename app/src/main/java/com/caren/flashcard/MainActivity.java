63b3b403db2964b1c298aa1ddb1151a9e85a2d1c
calren <calren24@gmail.com>
2018-09-02 14:55:45 -0700

    1. Show Answer when Question is clicked 2. Add Multiple Choice Answers

--- app/src/main/java/com/caren/flashcard/MainActivity.java
+++ app/src/main/java/com/caren/flashcard/MainActivity.java
@@ -2,12 +2,23 @@ package com.caren.flashcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView questionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        questionText = findViewById(R.id.question_text);
+        questionText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                questionText.setText("Barack Obama");
+            }
+        });
     }
 }
