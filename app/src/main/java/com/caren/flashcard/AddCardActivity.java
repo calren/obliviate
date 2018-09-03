12e7e718a34517ff94beb0f38b828a15b2aee8bc
calren <calren24@gmail.com>
2018-09-02 17:26:37 -0700

    Show newly created question after user hits 'save' button

--- app/src/main/java/com/caren/flashcard/AddCardActivity.java
+++ app/src/main/java/com/caren/flashcard/AddCardActivity.java
@@ -1,13 +1,39 @@
 package com.caren.flashcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddCardActivity extends AppCompatActivity {
+    public static final String QUESTION_KEY = "question";
+    public static final String WRONG_ANSWER_1_KEY = "wrong_answer_1";
+    public static final String WRONG_ANSWER_2_KEY = "wrong_answer_2";
+    public static final String CORRECT_ANSWER_KEY = "correct_answer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = ((EditText) findViewById(R.id.question_entry)).getText().toString();
+                String wrongAnswer1 = ((EditText) findViewById(R.id.answer_entry_1)).getText().toString();
+                String wrongAnswer2 = ((EditText) findViewById(R.id.answer_entry_2)).getText().toString();
+                String correctAnswer = ((EditText) findViewById(R.id.correct_answer_entry)).getText().toString();
+
+                Intent intent = new Intent();
+                intent.putExtra(QUESTION_KEY, question);
+                intent.putExtra(WRONG_ANSWER_1_KEY, wrongAnswer1);
+                intent.putExtra(WRONG_ANSWER_2_KEY, wrongAnswer2);
+                intent.putExtra(CORRECT_ANSWER_KEY, correctAnswer);
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/caren/flashcard/MainActivity.java
+++ app/src/main/java/com/caren/flashcard/MainActivity.java
@@ -8,13 +8,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView questionText;
+    public static final int REQUEST_CODE = 1;
+
+    private TextView questionText;
+    private TextView answerText1;
+    private TextView answerText2;
+    private TextView answerText3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         questionText = findViewById(R.id.question_text);
+        answerText1 = findViewById(R.id.answer_1);
+        answerText2 = findViewById(R.id.answer_2);
+        answerText3 = findViewById(R.id.answer_3);
+
         questionText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,8 +35,25 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.add_icon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                view.getContext().startActivity(new Intent(MainActivity.this, AddCardActivity.class));
+                Intent startAddCardActivityIntent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(startAddCardActivityIntent, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String question = data.getStringExtra(AddCardActivity.QUESTION_KEY);
+            String wrongAnswer1 = data.getStringExtra(AddCardActivity.WRONG_ANSWER_1_KEY);
+            String wrongAnswer2 = data.getStringExtra(AddCardActivity.WRONG_ANSWER_2_KEY);
+            String correctAnswer = data.getStringExtra(AddCardActivity.CORRECT_ANSWER_KEY);
+
+            questionText.setText(question);
+            answerText1.setText(wrongAnswer1);
+            answerText2.setText(wrongAnswer2);
+            answerText3.setText(correctAnswer);
+        }
     }
 }
